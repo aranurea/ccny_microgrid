553d80318ecf661f15c88859d17c2bdc7ed39b4f
aranurea <amandeep1028@icloud.com>
2018-11-28 10:07:11 -0500

    made get_hid run infinitely until inverter is found

--- get_hid.py
+++ get_hid.py
@@ -1,23 +1,31 @@
 import sys
 import os
+import time
 
 def get_hid():
-    os.system('dmesg > hidnum.txt')
-    infile = open('hidnum.txt', 'r')
-    inlines = infile.readlines()
-    infile.close()
-    os.system('rm hidnum.txt')
-    inlines.reverse()
-    vid = '0665'
-    pid = '5161'
-    hid = 'hidraw'
+    while 1:
+        os.system('dmesg > hidnum.txt')
+        infile = open('hidnum.txt', 'r')
+        inlines = infile.readlines()
+        infile.close()
+        os.system('rm hidnum.txt')
+        inlines.reverse()
+        vid = '0665'
+        pid = '5161'
+        hid = 'hidraw'
 
-    for line in inlines:
-        if (vid in line) and (pid in line) and (hid in line):
-            hid_pos = line.find('hidraw') + 6
-            while line[hid_pos]!= ':':
-                hidnum = hid + line[hid_pos]
-                hid_pos += 1
+        for line in inlines:
+            if (vid in line) and (pid in line) and (hid in line):
+                hid_pos = line.find('hidraw') + 6
+                while line[hid_pos]!= ':':
+                    hidnum = hid + line[hid_pos]
+                    hid_pos += 1
+                break
+
+        if hid is not 'hidraw':
             break
-    
+
+        print 'Device not found. Retry in 60 seconds.'
+        time.sleep(60)
+
     return hidnum
